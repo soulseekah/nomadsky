1d98eed6bda657c5b57411810c938da0a39aea96
Gennady Kovshenin <gennady@kovshenin.com>
2021-01-09 13:23:29 +0300

    Some more initial stuff

---
+++ src/Modifiers.gd
@@ -0,0 +1,65 @@
+class_name Modifiers
+
+
+# Where you are in the world
+class Location:
+	var cost: float
+	var bonus: float
+	
+	class Beijing extends Location:
+		func _init():
+			cost = 100
+			bonus = 10 
+
+
+# What you do your work on
+class Workstation:
+	var cost: float
+	var bonus: float
+
+	class Renovo extends Workstation:
+		func _init():
+			cost = 10
+			bonus = 0
+
+	class Wacbook extends Workstation:
+		func _init():
+			cost = 1000
+			bonus = 30
+
+
+# Where you live
+class Quarters:
+	var cost: float
+	var bonus: float
+	
+	class Hostel extends Quarters:
+		func _init():
+			cost = 100
+			bonus = 10
+
+	class Hotel extends Quarters:
+		func _init():
+			cost = 200
+			bonus = 20
+
+	class Apartment extends Quarters:
+		func _init():
+			cost = 150
+			bonus = 30
+
+
+# What your pet is
+class Pet:
+	var cost: float
+	var bonus: float
+	
+	class Cat extends Pet:
+		func _init():
+			cost = 10
+			bonus = 7
+
+	class Dog extends Pet:
+		func _init():
+			cost = 20
+			bonus = 14
---
+++ src/Nomad.gd
@@ -0,0 +1,25 @@
+class_name Nomad
+
+# C.R.E.A.M
+var money: int = 100
+
+# Properties
+var quarters: Modifiers.Quarters = null
+var pet: Modifiers.Pet = null
+var location: Modifiers.Location = Modifiers.Location.Beijing.new()
+var workstation: Modifiers.Workstation = Modifiers.Workstation.Renovo.new()
+
+
+# Skills
+var copywriting: int = 0
+var design: int = 0
+var code: int = 0
+var gamedev: int = 0
+var soft: int = 0
+
+
+# Stats
+var hunger: int = 0
+var health: int = 100
+var mood: int = 50
+var karma: int = 50
