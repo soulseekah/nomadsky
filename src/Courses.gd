e8d05e2f9e2bed825f2ff8fa1e9eaee9e39ee8e6
FitchChamp <sulenfo@gmail.com>
2021-01-13 01:39:24 +0300

    Courses done

--- src/Courses.gd
+++ src/Courses.gd
@@ -158,4 +158,106 @@ func _ready():
 				},
 			],
 		},
+		{
+			'name': 'Design: Bigginer', 'type': 'design', 'cost': 7,
+			'quiz': [
+				{
+					'question': 'What color should the text be?', 'answer': 'black on white background',
+					'filler': ['white on white background', 'red - you should read this'],
+				},
+				{
+					'question': 'What color is the taxi?', 'answer': 'yellow',
+					'filler': ['azure', 'like skittles'],
+				},
+				{
+					'question': 'What does the abbreviation rgb mean?', 'answer': 'red green blue',
+					'filler': ['robot good bot', "risotto gazpacho bacon"],
+				},
+			],
+		},
+		{
+			'name': 'Design: Cool lvl', 'type': 'design', 'cost': 15,
+			'quiz': [
+				{
+					'question': 'You want to place an image 4000x3000px what should I do with it?', 'answer': 'to reduce',
+					'filler': ['nothing', 'apply a beautiful filter'],
+				},
+				{
+					'question': 'How many shades of absolute black are there?', 'answer': '1',
+					'filler': ['3brg', '10000rgb'],
+				},
+				{
+					'question': 'What is a gradient?', 'answer': 'switching from one color to another',
+					'filler': ['pedestrian crossing', "both"],
+				},
+			],
+		},
+		{
+			'name': 'Gangsta of design', 'type': 'design', 'cost': 30,
+			'quiz': [
+				{
+					'question': 'What popular color scheme is used for correction?', 'answer': 'orange-blue',
+					'filler': ['blue light-blue dark-blue', 'blue. Why do something else?'],
+				},
+				{
+					'question': 'How many shades of gray are there in 50 shades of gray?', 'answer': '50',
+					'filler': ['1', 'I only remember the red room'],
+				},
+				{
+					'question': 'Is there an arrow on the Fedex logo?', 'answer': 'yes',
+					'filler': ['no', "there are 5 of them"],
+				},
+			],
+		},
+		{
+			'name': 'Beginner Dev: #;$', 'type': 'code', 'cost': 7,
+			'quiz': [
+				{
+					'question': What is the name of the longest key on the keyboard?', 'answer': 'space',
+					'filler': ['a', 'f13'],
+				},
+				{
+					'question': 'What is the name of the text markup language?', 'answer': 'html',
+					'filler': ['jaga', 'nomadsky'],
+				},
+				{
+					'question': 'What is the main thing in a computer after the CPU?', 'answer': 'to make it work',
+					'filler': ['heating for the cup', "enternet connection"],
+				},
+			],
+		},
+		{
+			'name': 'Intermediate Dev: Hello Darknet', 'type': 'code', 'cost': 15,
+			'quiz': [
+				{
+					'question': What programming language is like a snake?', 'answer': 'python',
+					'filler': ['worm', 'crocodile'],
+				},
+				{
+					'question': 'As a programmer, would you develop a computer game?', 'answer': 'no',
+					'filler': ['yeah, I'll make a lot of money lol', 'yes, I won't earn anything =('],
+				},
+				{
+					'question': 'You've been working all day and you're hungry:', 'answer': 'I'll order a pizza',
+					'filler': ['I'll cook something', "I'll have a coffee, I'm a hipster"],
+				},
+			],
+		},
+		{
+			'name': 'God Level Dev: 1010011110111111101011101100111001111001011100101110101111000011101000', 'type': 'code', 'cost': 30,
+			'quiz': [
+				{
+					'question': How many processors are there in an 8 core processor?', 'answer': '1',
+					'filler': ['8', '16 threads'],
+				},
+				{
+					'question': '11010001101001111111', 'answer': '11010001101001',
+					'filler': ['111100111001011110011', '11011101101111'],
+				},
+				{
+					'question': 'What hardware platform is the Raspberry Pi built on?', 'answer': 'arm',
+					'filler': ['amd', "x32"],
+				},
+			],
+		},
 	]: courses.append(Course.new(course))
